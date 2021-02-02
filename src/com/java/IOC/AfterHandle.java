63291e00e63b9dfa14bbdd4d66a5422609bd0d39
bangtidy <bangtidy@qq.com>
2021-02-02 22:44:21 +0800

    review spring 自动装配

--- src/com/java/IOC/AfterHandle.java
+++ src/com/java/IOC/AfterHandle.java
@@ -14,3 +14,4 @@ public class AfterHandle implements BeanPostProcessor {
         return null;
     }
 }
+
--- src/com/java/spring/mod/Person.java
+++ src/com/java/spring/mod/Person.java
@@ -31,6 +31,7 @@ public class Person {
                 '}';
     }
 
+
     public Person() {
     }
 
